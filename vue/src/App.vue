b5af56edf25fcf6caa972f8da9bc424f6c55c1ce
lies046 <skytree.skysky@gmail.com>
2022-06-11 18:25:22 +0900

    画面追加

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,8 @@
 <script>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <template>
-  <HelloWorld />
+  <router-view></router-view>
 </template>
