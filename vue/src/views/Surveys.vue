e2505eeb45e098c662e90c2c0b0c36c6192b66ad
lies046 <skytree.skysky@gmail.com>
2022-06-12 15:57:17 +0900

    追加ボタン設置

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,5 +1,27 @@
 <template>
-  <PageComponent title="Surveys">Content goes here</PageComponent>
+  <PageComponent>
+    <template v-slot:header>
+      <div class="flex justify-between items-center">
+      <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
+        <route-link
+          :to="{name: 'SurveyCreate'}"
+          class="
+          py-2
+          px-3
+          text-white
+          bg-emerald-500
+          rounded-md
+          hover:bg-emerald-600
+          "
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 -mt-1 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+          </svg>
+          Add new Survey
+        </route-link>
+      </div>
+    </template>
+  </PageComponent>
 </template>
 
 <script setup>
