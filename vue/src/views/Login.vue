1eb2342a12451ed7c74bf78119f802fdbe234f29
lies046 <skytree.skysky@gmail.com>
2022-06-12 14:55:43 +0900

    ログインdone

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,10 +6,21 @@
     <p class="mt-2 text-center text-sm text-gray-600">
       Or
       {{ ' ' }}
-      <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Register for free </router-link>
+      <router-link :to="{name: 'Register'}" class="font-medium text-indigo-600 hover:text-indigo-500"> Register for
+        free
+      </router-link>
     </p>
   </div>
   <form class="mt-8 space-y-6" @submit="login">
+    <div v-if="errorMsg" class="flex items-center justify-between py-3 px-5 bg-red-500 text-white rounded">
+      {{ errorMsg }}
+    <span @click="errorMsg = ''" class="w-8 h-8 flex items-center justify-center rounded-full transition-colors cursor-pointer hover:bg-[rgba(0,0,0,0.2)]">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+             stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+</svg>
+      </span>
+    </div>
     <input type="hidden" name="remember" value="true"/>
     <div class="rounded-md shadow-sm -space-y-px">
       <div>
@@ -53,6 +64,7 @@
 import {LockClosedIcon} from '@heroicons/vue/solid'
 import store from "../store";
 import {useRouter} from "vue-router";
+import {ref} from 'vue';
 
 const router = useRouter();
 const user = {
@@ -60,8 +72,9 @@ const user = {
   password: '',
   remember: false,
 }
+let errorMsg = ref('');
 
-function login(ev){
+function login(ev) {
   ev.preventDefault();
 
   store.dispatch('login', user)
@@ -69,6 +82,9 @@ function login(ev){
       router.push({
         name: 'Dashboard'
       })
+    })
+    .catch(err => {
+      errorMsg.value = err.response.data.error
     })
 }
 </script>
