58b0fcdb734dc568da8e0d264f15b9a74ed7fb32
lies046 <skytree.skysky@gmail.com>
2022-06-13 11:43:14 +0900

    form追加中

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -17,21 +17,23 @@
           </div>
           <div class="mt-1 flex items-center">
             <img v-if="model.image"
-              :src="model.image"
-              :alt="model.title"
-              class="w-64 h-48 object-cover"
+                 :src="model.image"
+                 :alt="model.title"
+                 class="w-64 h-48 object-cover"
             >
             <span
-            v-else
-            class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100"
+              v-else
+              class="flex items-center justify-center h-12 w-12 rounded-full overflow-hidden bg-gray-100"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-[80%] w-[80%] text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-</svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-[80%] w-[80%] text-gray-300" fill="none"
+                   viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+            </svg>
             </span>
             <button
-            type="button"
-            class="
+              type="button"
+              class="
               relative
               overflow-hidden
               ml-5
@@ -53,11 +55,71 @@
 
               ">
               <input type="file"
-              class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer">
+                     class="absolute left-0 top-0 right-0 bottom-0 opacity-0 cursor-pointer">
               Change
             </button>
           </div>
+          <div>
+            <label for="title" class="block text-sm font-medium text-gray-700">Title</label>
+            <input type="text" name="title" id="title" v-model="model.title" autocomplete="survey_title"
+                   class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
 
+          </div>
+
+          <div>
+            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+            <div class="mt-1">
+              <textarea id="description" name="description" rows="3" v-model="model.description"
+                        autocomplete="survey_description"
+                        class="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        placeholder="Describe your survey"></textarea>
+            </div>
+          </div>
+          <div>
+            <label for="expire_date" class="block text-sm font-medium text-gray-700">Expire Date</label>
+            <input type="date" name="expire_date" id="expire_date" v-model="model.expire_date"
+                   class="focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+          </div>
+
+          <div class="flex items-start">
+            <div class="flex items-center h-5">
+              <input type="checkbox" id="status" name="status" v-model="model.status"
+                     class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+            </div>
+            <div class="ml-3 text-sm">
+              <label for="status" class="font-medium text-gray-700">Active</label>
+            </div>
+          </div>
+        </div>
+        <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+          <h3 class="text-2xl font-semibold flex items-center justify-between">
+            Questions
+            <button type="button" @click="addQuestion()"
+                    class="flex items-center text-sm py-1 px-4 rounded-md text-white bg-gray-600 hover: bg-gray-700">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+              </svg>
+              Add Question
+            </button>
+          </h3>
+          <div v-if="!model.questions.length" class="text-center text-gray-600">
+            You don't have any questions created
+          </div>
+          <div v-for="(question, index) in model.questions" :key="question.id">
+            <QuestionEditor
+              :question="question"
+              :index="index"
+              @change="questionChange"
+              @addQuestion="addQuestion"
+              @deleteQuestion="deleteQuestion"
+            />
+          </div>
+        </div>
+        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button type="submit"
+                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Save
+          </button>
         </div>
       </div>
     </form>
@@ -66,10 +128,10 @@
 <script setup>
 import store from "../store";
 import {ref} from 'vue';
-import {useRouter} from "vue-router";
+import {useRoute} from "vue-router";
 import PageComponent from '../components/PageComponent.vue';
 
-const route = useRouter();
+const route = useRoute();
 
 let model = ref({
   title: "",
@@ -80,9 +142,9 @@ let model = ref({
   questions: []
 });
 
-// // if (route.params.id){
-// //   model.value = store.state.surveys.find(
-// //     (s) => s.id === parseInt(route.params.id)
-// //   );
-// }
+if (route.params.id){
+  model.value = store.state.surveys.find(
+    (s) => s.id === parseInt(route.params.id)
+  );
+}
 </script>
