e124714c3e2bf9ffa4c61a2f05ef62e71787d859
lies046 <skytree.skysky@gmail.com>
2022-06-26 10:49:20 +0900

    3:22:55

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\UpdateSurveyRequest;
 use App\Models\SurveyQuestion;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
@@ -93,6 +94,28 @@ class SurveyController extends Controller
         }
 
         $survey->update($data);
+
+        $existingIds = $survey->questions()->pluck('id')->toArray();
+        $newIds = Arr::pluck($data['questions'], 'id');
+        $toDelete = array_diff($existingIds, $newIds);
+        $toAdd = array_diff($newIds, $existingIds);
+
+        SurveyQuestion::destroy($toDelete);
+
+        foreach ($data['questions'] as $question){
+            if (in_array($question['id'], $toAdd)){
+                $question['survey_id'] = $survey->id;
+                $this->createQuestion($question);
+            }
+        }
+
+        $questionMap = collect($data['questions'])->keyBy('id');
+
+        foreach ($survey->questions as $question){
+            if (isset($questionMap[$question->id])){
+                $this->updateQuestion($question, $questionMap[$question->id]);
+            }
+        }
         return new SurveyResource($survey);
     }
 
@@ -172,5 +195,28 @@ class SurveyController extends Controller
             'survey_id' => 'exists:App\Models\Survey,id'
         ]);
         return SurveyQuestion::create($validator->validated());
+    }
+
+    private function updateQuestion(SurveyQuestion $question, $data)
+    {
+        if (is_array($data['data'])){
+            $data['data'] = json_encode($data['data']);
+        }
+        $validator = Validator::make($data, [
+            'id' => 'exists:App\Models\SurveyQuestion,id',
+            'question' => 'required|string',
+            'type' => ['required', Rule::in([
+                Survey::TYPE_TEXT,
+                Survey::TYPE_TEXTAREA,
+                Survey::TYPE_SELECT,
+                Survey::TYPE_RADIO,
+                Survey::TYPE_CHECKBOX
+            ])],
+            'description' => 'nullable|string',
+            'data' => 'present',
+            ]);
+
+        return $question->update($validator->validated());
+
     }
 }
